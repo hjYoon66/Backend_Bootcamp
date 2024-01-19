9f1526f4d5255c6ec6e826bebad9ee2e64601f78
hjyoon66 <poiu601@naver.com>
2024-01-20 03:32:05 +0900

    Project Setting :smiley:

--- src/main/java/com/yoon/dao/MovieDAO.java
+++
@@ -1,8 +0,0 @@
-package com.yoon.dao;
-import java.util.List;
-import com.yoon.vo.MovieVO;
- 
-public interface MovieDAO {
-    public List<MovieVO> selectMovie() throws Exception;
-    public List<MovieVO> findMovieByName(String movieName) throws Exception;
-}
--- src/main/java/com/yoon/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.yoon.service;
-
-import java.util.List;
-import com.yoon.vo.MovieVO;
- 
-public interface MovieService {
-    public List<MovieVO> selectMovie() throws Exception;
-    public List<MovieVO> findMovieByName(String movieName) throws Exception;
-}
